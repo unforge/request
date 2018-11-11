c0a1a7ca2c8de97573c7aee369f553fa8df84226
Ronam Unstirred <unforge.coder@gmail.com>
2018-11-11 17:00:39 +0700

    Added methods to get data from POST, GET, COOKIE

--- src/Tools.php
+++ src/Tools.php
@@ -18,5 +18,442 @@ namespace Unforge\RequestToolkit;
  */
 final class Tools
 {
+    /**
+     * @return bool
+     */
+    public static function checkExistPost() : bool
+    {
+        return (bool) $_SERVER['REQUEST_METHOD'] == 'POST';
+    }
 
+    /**
+     * Return ALL params from POST
+     *
+     * @return array
+     */
+    public static function getAllFromPost() : array
+    {
+        return $_POST ?? [];
+    }
+
+    /**
+     * Return INT value from POST
+     *
+     * @param string $key
+     * @param bool|int $default
+     *
+     * @return bool|int
+     */
+    public static function getIntFromPost(string $key, $default = false)
+    {
+        if (isset($_POST[$key])) {
+            return (int) $_POST[$key];
+        }
+
+        return $default;
+    }
+
+    /**
+     * Return FLOAT value from POST
+     *
+     * @param string $key
+     * @param bool $default
+     *
+     * @return bool|float
+     */
+    public static function getFloatFromPost(string $key, $default = false)
+    {
+        if (isset($_POST[$key])) {
+            return (float) $_POST[$key];
+        }
+
+        return $default;
+    }
+
+    /**
+     * Return STRING value from POST
+     *
+     * @param string $key
+     * @param bool $default
+     *
+     * @return bool|string
+     */
+    public static function getStringFromPost(string $key, $default = false)
+    {
+        if (isset($_POST[$key])) {
+            return (string) trim($_POST[$key]);
+        }
+
+        return $default;
+    }
+
+    /**
+     * Return ARRAY value from POST
+     *
+     * @param string $key
+     * @param bool $default
+     *
+     * @return bool|array
+     */
+    public static function getArrayFromPost(string $key, $default = false)
+    {
+        if (isset($_POST[$key])) {
+            return (array) $_POST[$key];
+        }
+
+        return $default;
+    }
+
+    /**
+     * Return RAW value from POST
+     *
+     * @param string $key
+     * @param bool $default
+     *
+     * @return bool|mixed
+     */
+    public static function getRawFromPost(string $key, $default = false)
+    {
+        if (isset($_POST[$key])) {
+            return $_POST[$key];
+        }
+
+        return $default;
+    }
+
+    /**
+     * @return bool
+     */
+    public static function checkExistGet() : bool
+    {
+        return (bool) $_SERVER['REQUEST_METHOD'] == 'GET';
+    }
+
+    /**
+     * Return ALL params from GET
+     *
+     * @return array
+     */
+    public static function getAllFromGet() : array
+    {
+        return $_GET ?? [];
+    }
+
+    /**
+     * Return INT value from GET
+     *
+     * @param string $key
+     * @param bool $default
+     *
+     * @return bool|int
+     */
+    public static function getIntFromGet(string $key, $default = false)
+    {
+        if (isset($_GET[$key])) {
+            return (int) $_GET[$key];
+        }
+
+        return $default;
+    }
+
+    /**
+     * Return FLOAT value from GET
+     *
+     * @param string $key
+     * @param bool $default
+     *
+     * @return bool|float
+     */
+    public static function getFloatFromGet(string $key, $default = false)
+    {
+        if (isset($_GET[$key])) {
+            return (float) $_GET[$key];
+        }
+
+        return $default;
+    }
+
+    /**
+     * Return STRING value from GET
+     *
+     * @param string $key
+     * @param bool $default
+     *
+     * @return bool|string
+     */
+    public static function getStringFromGet(string $key, $default = false)
+    {
+        if (isset($_GET[$key])) {
+            return (string) trim($_GET[$key]);
+        }
+
+        return $default;
+    }
+
+    /**
+     * Return ARRAY value from GET
+     *
+     * @param string $key
+     * @param bool $default
+     *
+     * @return bool|array
+     */
+    public static function getArrayFromGet(string $key, $default = false)
+    {
+        if (isset($_GET[$key])) {
+            return (array) $_GET[$key];
+        }
+
+        return $default;
+    }
+
+    /**
+     * Return RAW value from GET
+     *
+     * @param string $key
+     * @param bool $default
+     *
+     * @return bool|mixed
+     */
+    public static function getRawFromGet(string $key, $default = false)
+    {
+        if (isset($_GET[$key])) {
+            return $_GET[$key];
+        }
+
+        return $default;
+    }
+
+    /**
+     * @return bool
+     */
+    public static function checkExistRequest() : bool
+    {
+        $post = self::checkExistPost();
+
+        if ($post === false) {
+            return self::checkExistGet();
+        }
+
+        return $post;
+    }
+
+    /**
+     * Return ALL params from POST or GET
+     *
+     * @return array
+     */
+    public static function getAllFromRequest() : array
+    {
+        $post = self::getAllFromPost();
+
+        if (empty($post)) {
+            return self::getAllFromGet();
+        }
+
+        return $post;
+    }
+
+    /**
+     * Return INT value from POST or GET
+     *
+     * @param string $key
+     * @param bool $default
+     *
+     * @return bool|int
+     */
+    public static function getIntFromRequest(string $key, $default = false)
+    {
+        $post = self::getIntFromPost($key);
+
+        if ($post === false) {
+            return self::getIntFromGet($key, $default);
+        }
+
+        return $post;
+    }
+
+    /**
+     * Return FLOAT value from POST or GET
+     *
+     * @param string $key
+     * @param bool $default
+     *
+     * @return bool|float
+     */
+    public static function getFloatFromRequest(string $key, $default = false)
+    {
+        $post = self::getFloatFromPost($key);
+
+        if ($post === false) {
+            return self::getFloatFromGet($key, $default);
+        }
+
+        return $post;
+    }
+
+    /**
+     * Return STRING value from POST or GET
+     *
+     * @param string $key
+     * @param bool $default
+     *
+     * @return bool|string
+     */
+    public static function getStringFromRequest(string $key, $default = false)
+    {
+        $post = self::getStringFromPost($key);
+
+        if ($post === false) {
+            return self::getStringFromGet($key, $default);
+        }
+
+        return $post;
+    }
+
+    /**
+     * Return ARRAY value from POST or GET
+     *
+     * @param string $key
+     * @param bool $default
+     *
+     * @return bool|array
+     */
+    public static function getArrayFromRequest(string $key, $default = false)
+    {
+        $post = self::getArrayFromPost($key);
+
+        if ($post === false) {
+            return self::getArrayFromGet($key, $default);
+        }
+
+        return $post;
+    }
+
+    /**
+     * Return RAW value from POST or GET
+     *
+     * @param string $key
+     * @param bool $default
+     *
+     * @return bool|array
+     */
+    public static function getRawFromRequest(string $key, $default = false)
+    {
+        $post = self::getRawFromPost($key);
+
+        if ($post === false) {
+            return self::getRawFromGet($key, $default);
+        }
+
+        return $post;
+    }
+
+    // TODO Add support prepare request methods from PUT
+
+    /**
+     * @param string $key
+     * @return bool
+     */
+    public static function checkExistsCookie(string $key = '') : bool
+    {
+        if (!$key) {
+            return (bool) isset($_COOKIE);
+        }
+
+        return (bool) isset($_COOKIE[$key]);
+    }
+
+    /**
+     * Return ALL params from COOKIE
+     *
+     * @return array
+     */
+    public static function getAllFromCookie() : array
+    {
+        return $_COOKIE ?? [];
+    }
+
+    /**
+     * Return INT value from COOKIE
+     *
+     * @param string $key
+     * @param bool $default
+     *
+     * @return bool|int
+     */
+    public static function getIntFromCookie(string $key, $default = false)
+    {
+        if (isset($_COOKIE[$key])) {
+            return (int) $_COOKIE[$key];
+        }
+
+        return $default;
+    }
+
+    /**
+     * Return FLOAT value from COOKIE
+     *
+     * @param string $key
+     * @param bool $default
+     *
+     * @return bool|float
+     */
+    public static function getFloatFromCookie(string $key, $default = false)
+    {
+        if (isset($_COOKIE[$key])) {
+            return (float) $_COOKIE[$key];
+        }
+
+        return $default;
+    }
+
+    /**
+     * Return STRING value from COOKIE
+     *
+     * @param string $key
+     * @param bool $default
+     *
+     * @return bool|string
+     */
+    public static function getStringFromCookie(string $key, $default = false)
+    {
+        if (isset($_COOKIE[$key])) {
+            return (string) trim($_COOKIE[$key]);
+        }
+
+        return $default;
+    }
+
+    /**
+     * Return ARRAY value from COOKIE
+     *
+     * @param string $key
+     * @param bool $default
+     *
+     * @return bool|array
+     */
+    public static function getArrayFromCookie(string $key, $default = false)
+    {
+        // TODO add cookie data converter to array
+
+        return $default;
+    }
+
+    /**
+     * Return RAW value FROM COOKIE
+     *
+     * @param string $key
+     * @param bool $default
+     *
+     * @return bool|mixed
+     */
+    public static function getRawFromCookie(string $key, $default = false)
+    {
+        if (isset($_COOKIE[$key])) {
+            return $_COOKIE[$key];
+        }
+
+        return $default;
+    }
 }
